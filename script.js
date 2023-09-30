ece10e46a6fab941eef39685daa465ccb22a4f2e
christian goddu <rgoddu909@gmail.com>
2023-09-29 20:33:05 -0400

    added function

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+ 
   function generatePassword() {
-
 
     //prompts user to make selection of necessary criteria to include in password
 
@@ -31,13 +31,12 @@ function writePassword() {
       return
     }
 
-
-
     //Different lists of possible characters
     specialList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
     numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     uppercaseList = lowercaseList.map(function (x) { return x.toUpperCase(); })
+    console.log(specialList)
 
     //creates an array to randomly choose the number of characters from the prompt, making sure that all password criteria are met
     var unscrambledArray = []
@@ -47,43 +46,26 @@ function writePassword() {
 
     // adds allowable characters to a master list *
     // checks to make sure all 4 criteria are met (assuming the user chose 'Ok' on all 4)
-    if (needsSpecial) {
-      possibleCharacters = possibleCharacters.concat(specialList)
-
-      var randomSpecial = specialList[Math.floor(Math.random() * specialList.length)]
-      unscrambledArray.push(randomSpecial)
+    function meetCriteria(needsCriteria, criteriaList){
+      if (needsCriteria) {
+        possibleCharacters = possibleCharacters.concat(criteriaList)
+    
+        var randomz = criteriaList[Math.floor(Math.random() * criteriaList.length)]
+        unscrambledArray.push(randomz)
+      }
     }
+    meetCriteria(needsNumeric, numberList)
+    meetCriteria(needsLowercase, lowercaseList)
+    meetCriteria(needsUppercase, uppercaseList)
+    meetCriteria(needsSpecial, specialList)
 
-    if (needsLowercase) {
-      possibleCharacters = possibleCharacters.concat(lowercaseList)
-
-      var randomLowercase = lowercaseList[Math.floor(Math.random() * lowercaseList.length)]
-      unscrambledArray.push(randomLowercase)
-    }
-
-    if (needsUppercase) {
-      possibleCharacters = possibleCharacters.concat(uppercaseList)
-
-      var randomUppercase = uppercaseList[Math.floor(Math.random() * uppercaseList.length)]
-      unscrambledArray.push(randomUppercase)
-    }
-
-    if (needsNumeric) {
-      possibleCharacters = possibleCharacters.concat(numberList)
-
-      var randomNumeric = numberList[Math.floor(Math.random() * numberList.length)]
-      unscrambledArray.push(randomNumeric)
-    }
-
-    // specialList.concat(numberList.concat(lowercaseList.concat(uppercaseList)))
+    // fills the rest of the array with randomly selected characters
     for (x = unscrambledArray.length; x < passwordLength; x++) {
 
-
-      // fills the rest of the array with randomly selected characters
-      var randomCharacter = possibleCharacters[(Math.floor(Math.random() * possibleCharacters.length))]
+        var randomCharacter = possibleCharacters[(Math.floor(Math.random() * possibleCharacters.length))]
       unscrambledArray.push(randomCharacter)
-
     }
+
     // scrambles the array so that the order isn't predictable
     scrambledArray = []
     for (x = 0; x < passwordLength; x++) {
@@ -94,15 +76,10 @@ function writePassword() {
 
     //transforms the array into a string to output
     passwordOutput = scrambledArray.join("")
-    alert("Password: " + passwordOutput)
-
-    // WHEN all prompts are answered
-    // THEN a password is generated that matches the selected criteria
-    // WHEN the password is generated
-    // THEN the password is either displayed in an alert or written to the page
+    alert("Password: " + passwordOutput + "\n" + scrambledArray.length)
+    console.log(scrambledArray.length)
   }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-debugger
